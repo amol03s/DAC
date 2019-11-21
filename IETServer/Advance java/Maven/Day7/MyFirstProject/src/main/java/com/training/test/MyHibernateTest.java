260aa5549facf5050462aa8c497cdd80286d8200
amol03s <amolthawal4444@gmail.com>
2019-11-21 20:29:43 +0530

    Maven

---
+++ IETServer/Advance java/Maven/Day7/MyFirstProject/src/main/java/com/training/test/MyHibernateTest.java
@@ -0,0 +1,30 @@
+package com.training.test;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.training.beans.MyUser;
+import com.training.service.UserService;
+import com.training.service.UserServiceImpl;
+
+public class MyHibernateTest {
+	public static void main(String[] args) {
+		MyUser ob=new MyUser(12,"Kishori","1111","aaa@gmail.com");
+		MyUser ob1=new MyUser(13,"Rajan","2222","bbb@gmail.com");
+		UserService us=new UserServiceImpl();
+		List<MyUser> arr=new ArrayList<>();
+		arr.add(ob);
+		arr.add(ob1);
+		boolean ans=us.addUser(arr);
+		if(ans) {
+			System.out.println("User added Successfully");
+			
+		}
+		else {
+			System.out.println("Error occured");
+		}
+		
+		
+		
+	}
+
+}
